69b37c8aae6c2237a7532b0601408cae08621ab5
guangnianruoxu <1623194994@qq.com>
2024-09-03 14:43:52 +0800

    write my first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line2 = '<img src="https://img0.baidu.com/it/u=672126781,3294203100&fm=253&fmt=auto&app=120&f=JPEG?w=873&h=500">'
+    return HttpResponse(line1+line2)
+
