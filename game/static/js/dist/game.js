3886031ded6846f93f29b470ce762cc86b271cf1
guangnianruoxu <1623194994@qq.com>
2024-09-21 14:35:45 +0800

    implement acapp acwing auth2 login system

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -572,8 +572,12 @@ class Settings {
     }
 
     start(){
-        this.getinfo();
-        this.add_listening_events();
+        if (this.platform === "ACAPP"){
+            this.getinfo_acapp();
+        } else {
+            this.getinfo_web();
+            this.add_listening_events();
+        }
     }
 
     add_listening_events(){
@@ -694,7 +698,35 @@ class Settings {
         this.$login.show();
     }
 
-    getinfo(){
+    acapp_login(appid,redirect_uri,scope,state){
+        let outer = this;
+
+        this.root.AcWingOS.api.oauth2.authorize(appid, redirect_uri, scope, state, function(resp){
+            console.log("called from acapp_login function")
+            console.log(resp);
+            if(resp.result === "success"){
+                outer.username = resp.username;
+                outer.photo = resp.photo;
+                outer.hide();
+                outer.root.menu.show();
+            }
+        });
+    }
+
+    getinfo_acapp(){
+        let outer = this;
+        $.ajax({
+            url: "https://app7133.acapp.acwing.com.cn/settings/acwing/acapp/apply_code/",
+            type: "GET",
+            success: function(resp){
+                if (resp.result === "success"){
+                    outer.acapp_login(resp.appid,resp.redirect_uri,resp.scope,resp.state);
+                }
+            }
+        })
+    }
+
+    getinfo_web(){
         let outer = this;
 
         $.ajax({
@@ -727,6 +759,7 @@ class Settings {
 }
 export class AcGame {
     constructor(id, AcWingOS) {
+        console.log(AcWingOS);
         this.id = id;
         this.$ac_game = $('#' + id);
         this.AcWingOS = AcWingOS;
